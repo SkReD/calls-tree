3a54ea2cd685c3bb1b88e1f00cc729487706218c
Michail Shipov <mshipov@yandex.ru>
2019-05-24 23:49:57 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { pushCall } from './stackUtils/pushCall'
 export { popCall } from './stackUtils/popCall'
-export { default as babelPlugin } from './babelPlugin'
+export { default as babelPlugin } from './babelPluginn'
